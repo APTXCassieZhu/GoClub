9e03ec84d03c5bd693c5a9035d057cd51270d744
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-05-03 20:45:25 -0400

    change password

--- clubGoGo/src/main/java/clubgogo/domain/club.java
+++ clubGoGo/src/main/java/clubgogo/domain/club.java
@@ -10,19 +10,30 @@ public class Club {
     private String logopath;
     private String description;
     private String email;
-    // private String event_location;
-    // private String event_time;
-    // private String event_name;
+    private String event_location;
+    private String event_time;
+    private String event_name;
 
     public Club(){
 
     }
 
-    public Club(String clubname, String logopath,String description, String email) {
+    public Club(String clubname, String logopath, String description, String email) {
         this.clubname = clubname;
         this.logopath = logopath;
         this.description = description;
         this.email = email;
+    }
+
+    public Club(String clubname, String logopath, String description, 
+    String email, String event_location, String event_time, String event_name) {
+        this.clubname = clubname;
+        this.logopath = logopath;
+        this.description = description;
+        this.email = email;
+        this.event_location = event_location;
+        this.event_location = event_time;
+        this.event_location = event_name;    
     }
 
     public String getClubname() {
@@ -57,6 +68,30 @@ public class Club {
         this.email = email;
     }
 
+    public String getEvent_location() {
+        return event_location;
+    }
+
+    public void setEvent_location(String event_location) {
+        this.event_location = event_location;
+    }
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+
+    public String getEvent_time() {
+        return event_time;
+    }
+
+    public void setEvent_time(String event_time) {
+        this.event_time = event_time;
+    }
+
     @Override
     public String toString() {
         return "club{" +
@@ -64,6 +99,9 @@ public class Club {
                 ", logopath='" + logopath + '\'' +
                 ", description='" + description + '\'' +
                 ", email='" + email + '\'' +
+                ", event_location='" + event_location + '\'' +
+                ", event_name='" + event_name + '\'' +
+                ", event_time='" + event_time + '\'' +
                 '}';
     }
 
